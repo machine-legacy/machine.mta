808cc8c84c310b02a237b15fe4493015eb44d3e8
Jacob Lewallen <jlewallen@gmail.com>
2008-12-01 23:56:38 -0800

    Adding MessagePayload class.
    This just wraps a byte array and includes a type converter for going
    between strings and MessagePayload's so that we can include them on
    messages and serialize them properly.

---
+++ Source/Machine.Mta/MessagePayload.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Machine.Mta
+{
+  public class MessagePayloadConverter : TypeConverter
+  {
+    public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+    {
+      return typeof(String).IsAssignableFrom(sourceType);
+    }
+
+    public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+    {
+      return new MessagePayload(Convert.FromBase64String(value.ToString()));
+    }
+
+    public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+    {
+      return typeof(String).IsAssignableFrom(destinationType);
+    }
+
+    public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+    {
+      MessagePayload payload = (MessagePayload)value;
+      return Convert.ToBase64String(payload.ToByteArray());
+    }
+  }
+  [TypeConverter(typeof(MessagePayloadConverter))]
+  public class MessagePayload
+  {
+    private readonly byte[] _data;
+
+    public byte[] ToByteArray()
+    {
+      return _data;
+    }
+
+    public MessagePayload(byte[] data)
+    {
+      _data = data;
+    }
+  }
+}
