7c907d30f2860302a59d74c98ed0ffd20414fb7b
Jacob Lewallen <jlewallen@gmail.com>
2008-12-18 13:44:06 -0800

    Handy methods for scheduling stuff Now+Time.

--- Source/Machine.Mta/Timing/ServerClock.cs
+++ Source/Machine.Mta/Timing/ServerClock.cs
@@ -6,5 +6,20 @@ namespace Machine.Mta.Timing
   public class ServerClock
   {
     public static Func<DateTime> Now = () => DateTime.UtcNow;
+
+    public static DateTime Later(TimeSpan time)
+    {
+      return Now() + time;
+    }
+
+    public static DateTime SecondsLater(double seconds)
+    {
+      return Later(TimeSpan.FromSeconds(seconds));
+    }
+
+    public static DateTime MinutesLater(double minutes)
+    {
+      return Later(TimeSpan.FromMinutes(minutes));
+    }
   }
 }
