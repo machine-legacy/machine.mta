33dc953f858c32c8c7e27b32182fb05ae2614e85
Jacob Lewallen <jlewallen@gmail.com>
2008-12-21 22:50:02 -0800

    EndpointResolver looks in the container for factories and tries those.
    When one returns a valid IEndpoint we cache that value for future lookyups.

--- Source/Machine.Mta/Internal/EndpointResolver.cs
+++ Source/Machine.Mta/Internal/EndpointResolver.cs
@@ -1,17 +1,50 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using Machine.Container.Services;
+using Machine.Core.Utility;
 
 namespace Machine.Mta.Internal
 {
+  public interface IEndpointFactory
+  {
+    IEndpoint CreateEndpoint(EndpointName name);
+  }
   public interface IEndpointResolver
   {
     IEndpoint Resolve(EndpointName name);
   }
   public class EndpointResolver : IEndpointResolver
   {
+    readonly Dictionary<EndpointName, IEndpoint> _cache = new Dictionary<EndpointName, IEndpoint>();
+    readonly ReaderWriterLock _lock = new ReaderWriterLock();
+    readonly IMachineContainer _container;
+
+    public EndpointResolver(IMachineContainer container)
+    {
+      _container = container;
+    }
+
     public IEndpoint Resolve(EndpointName name)
     {
-      throw new System.NotImplementedException();
+      using (RWLock.AsReader(_lock))
+      {
+        if (_cache.ContainsKey(name))
+        {
+          return _cache[name];
+        }
+        foreach (IEndpointFactory factory in _container.Resolve.All<IEndpointFactory>())
+        {
+          IEndpoint resolved = factory.CreateEndpoint(name);
+          if (resolved != null)
+          {
+            _lock.UpgradeToWriterLock(Timeout.Infinite);
+            _cache[name] = resolved;
+            return resolved;
+          }
+        }
+      }
+      throw new InvalidOperationException();
     }
   }
 }
