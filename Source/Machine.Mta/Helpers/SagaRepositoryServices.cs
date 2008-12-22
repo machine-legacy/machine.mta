89021cf7cb6ea422de79170e184cbcb651fb43e9
Jacob Lewallen <jlewallen@gmail.com>
2008-12-22 09:55:31 -0800

    Service collection for saga repositories.

---
+++ Source/Machine.Mta/Helpers/SagaRepositoryServices.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Reflection;
+
+using Machine.Container;
+using Machine.Container.Plugins;
+using Machine.Mta.Internal;
+using Machine.Mta.Sagas;
+
+namespace Machine.Mta.Helpers
+{
+  public class SagaRepositoryServices : IServiceCollection
+  {
+    readonly Assembly[] _assemblies;
+
+    public SagaRepositoryServices(params Assembly[] assemblies)
+    {
+      _assemblies = assemblies;
+    }
+
+    public void RegisterServices(ContainerRegisterer register)
+    {
+      foreach (Assembly assembly in _assemblies)
+      {
+        foreach (Type type in assembly.GetExportedTypes())
+        {
+          if (type.IsImplementationOfGenericType(typeof(ISagaStateRepository<>)))
+          {
+            register.Type(type);
+          }
+        }
+      }
+    }
+  }
+}
