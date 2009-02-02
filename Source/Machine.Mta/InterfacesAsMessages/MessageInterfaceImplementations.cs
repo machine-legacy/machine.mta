cb64e1778cc93c52f384aef670ce0d608f315cf1
Jacob Lewallen <jlewallen@gmail.com>
2009-02-02 09:32:07 -0800

    Improving message interface registration/creation.
    Allows for multiple calls to register groups of message interfaces. When
    a message type is first resolved we'll generate the implementations and
    disallow any future registrations.

--- Source/Machine.Mta/InterfacesAsMessages/MessageInterfaceImplementations.cs
+++ Source/Machine.Mta/InterfacesAsMessages/MessageInterfaceImplementations.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
+
+using Machine.Core.Utility;
 
 namespace Machine.Mta.InterfacesAsMessages
 {
   public class MessageInterfaceImplementations
   {
+    static readonly log4net.ILog _log = log4net.LogManager.GetLogger(typeof(MessageInterfaceImplementations));
+    readonly List<Type> _messageTypes = new List<Type>();
     readonly Dictionary<Type, Type> _interfaceToClass = new Dictionary<Type, Type>();
     readonly Dictionary<Type, Type> _classToInterface = new Dictionary<Type, Type>();
     readonly IMessageInterfaceImplementationFactory _messageInterfaceImplementationFactory;
+    readonly ReaderWriterLock _lock = new ReaderWriterLock();
+    bool _generated;
 
     public MessageInterfaceImplementations(IMessageInterfaceImplementationFactory messageInterfaceImplementationFactory)
     {
@@ -21,26 +28,61 @@ namespace Machine.Mta.InterfacesAsMessages
 
     public void GenerateImplementationsOf(IEnumerable<Type> types)
     {
-      foreach (KeyValuePair<Type, Type> generated in _messageInterfaceImplementationFactory.ImplementMessageInterfaces(types))
+      using (RWLock.AsWriter(_lock))
       {
-        _interfaceToClass[generated.Key] = generated.Value;
-        _classToInterface[generated.Value] = generated.Key;
+        if (_generated)
+        {
+          throw new InvalidOperationException("Message interfaces already implemented!");
+        }
+        foreach (Type type in types)
+        {
+          if (!_messageTypes.Contains(type))
+          {
+            _messageTypes.Add(type);
+          }
+        }
       }
     }
 
     public Type GetClassFor(Type type)
     {
-      return _interfaceToClass[type];
+      using (RWLock.AsReader(_lock))
+      {
+        GenerateIfNecessary();
+        return _interfaceToClass[type];
+      }
     }
 
     public Type GetInterfaceFor(Type type)
     {
-      return _classToInterface[type];
+      using (RWLock.AsReader(_lock))
+      {
+        GenerateIfNecessary();
+        return _classToInterface[type];
+      }
     }
 
     public bool IsClassOrInterface(Type type)
     {
-      return _interfaceToClass.ContainsKey(type) || _classToInterface.ContainsKey(type);
+      using (RWLock.AsReader(_lock))
+      {
+        GenerateIfNecessary();
+        return _interfaceToClass.ContainsKey(type) || _classToInterface.ContainsKey(type);
+      }
+    }
+
+    private void GenerateIfNecessary()
+    {
+      if (RWLock.UpgradeToWriterIf(_lock, () => !_generated))
+      {
+        _log.Info("Generating");
+        foreach (KeyValuePair<Type, Type> generated in _messageInterfaceImplementationFactory.ImplementMessageInterfaces(_messageTypes))
+        {
+          _interfaceToClass[generated.Key] = generated.Value;
+          _classToInterface[generated.Value] = generated.Key;
+        }
+        _generated = true;
+      }
     }
   }
 }
