bcdce6312ab2726860d378c39d9ca696b36cba35
Jacob Lewallen <jlewallen@gmail.com>
2009-02-04 01:27:50 -0800

    Simple task factory that resolves from the container.

--- Source/Machine.Mta/Timing/TimingTaskFactory.cs
+++ Source/Machine.Mta/Timing/TimingTaskFactory.cs
@@ -1,16 +1,24 @@
 using System;
+using Machine.Container.Services;
 
 namespace Machine.Mta.Timing
 {
   public class TimingTaskFactory
   {
+    readonly IMachineContainer _container;
     readonly IMessageBus _bus;
     readonly IMessageFactory _factory;
 
-    public TimingTaskFactory(IMessageBus bus, IMessageFactory factory)
+    public TimingTaskFactory(IMessageBus bus, IMessageFactory factory, IMachineContainer container)
     {
       _bus = bus;
+      _container = container;
       _factory = factory;
+    }
+
+    public IOnceASecondTask Task<T>() where T : IOnceASecondTask
+    {
+      return _container.Resolve.Object<T>();
     }
 
     public IOnceASecondTask PublishMessage<T>(ITrigger trigger) where T : class, IMessage
