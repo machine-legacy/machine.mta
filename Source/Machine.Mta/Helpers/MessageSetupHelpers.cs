8c7836bc6caef2788cc2b889aad27c2475973ddb
Jacob Lewallen <jlewallen@gmail.com>
2009-02-04 10:47:28 -0800

    Message setup helpers operate on IMessageRegisterer.

--- Source/Machine.Mta/Helpers/MessageSetupHelpers.cs
+++ Source/Machine.Mta/Helpers/MessageSetupHelpers.cs
@@ -2,13 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 
-using Machine.Mta.InterfacesAsMessages;
-
 namespace Machine.Mta.Helpers
 {
   public static class MessageSetupHelpers
   {
-    public static void RegisterMessageTypes(this MessageInterfaceImplementations implementations, params Assembly[] assemblies)
+    public static void AddMessageTypes(this IMessageRegisterer registerer, params Assembly[] assemblies)
     {
       List<Type> types = new List<Type>();
       foreach (Assembly assembly in assemblies)
@@ -21,7 +19,7 @@ namespace Machine.Mta.Helpers
           }
         }
       }
-      implementations.AddMessageTypes(types);
+      registerer.AddMessageTypes(types);
     }
   }
 }
