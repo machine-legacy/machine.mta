59acbeafac012ccfd5cbfa1114541828dda8d58d
Jacob Lewallen <jlewallen@gmail.com>
2009-04-20 13:45:11 -0700

    Serialization binder that let's us use IMessage implementations.
    Not totally happy with how this is being done for now....

--- Source/Machine.Mta/Serializers.cs
+++ Source/Machine.Mta/Serializers.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -19,7 +20,7 @@ namespace Machine.Mta
     { 
       _binaryFormatter = new BinaryFormatter();
       _binaryFormatter.AssemblyFormat = FormatterAssemblyStyle.Simple;
-      _binaryFormatter.Binder = new TransportMessageBinder();
+      _binaryFormatter.Binder = new SerializationBinders(new TransportMessageBinder(), new GeneratedMessageTypeBinder());
       _jsonSerializer = new JsonSerializer();
       _jsonSerializer.Converters.Add(new EndpointAddressJsonConverter());
       _jsonSerializer.Converters.Add(new ExceptionJsonConverter());
@@ -64,6 +65,58 @@ namespace Machine.Mta
       if (typeName == typeof(EndpointAddress).FullName)
       {
         return typeof(EndpointAddress);
+      }
+      return null;
+    }
+  }
+
+  public class GeneratedMessageTypeBinder : SerializationBinder
+  {
+    readonly string _messagesAssemblyName = "Messages, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
+    Assembly _messagesAssembly;
+
+    public override Type BindToType(string assemblyName, string typeName)
+    {
+      if (assemblyName != _messagesAssemblyName)
+      {
+        return null;
+      }
+      if (_messagesAssembly == null)
+      {
+        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+        {
+          if (assembly.FullName == _messagesAssemblyName)
+          {
+            _messagesAssembly = assembly;
+          }
+        }
+        if (_messagesAssembly == null)
+        {
+          throw new InvalidOperationException("Unable to locate Messages assembly dynamically.");
+        }
+      }
+      return _messagesAssembly.GetType(typeName);
+    }
+  }
+
+  public class SerializationBinders : SerializationBinder
+  {
+    readonly SerializationBinder[] _binders;
+
+    public SerializationBinders(params SerializationBinder[] binders)
+    {
+      _binders = binders;
+    }
+
+    public override Type BindToType(string assemblyName, string typeName)
+    {
+      foreach (SerializationBinder binder in _binders)
+      {
+        Type resolved = binder.BindToType(assemblyName, typeName);
+        if (resolved != null)
+        {
+          return resolved;
+        }
       }
       return null;
     }
