b9df4a900ce700c64af00b5f07bdc1a6613d893e
Jacob Lewallen <jlewallen@gmail.com>
2009-06-02 17:35:45 -0700

    Logging subscription messages from the static subscription service.

--- Source/Machine.Mta.NServiceBus/StaticSubscriptionStorage.cs
+++ Source/Machine.Mta.NServiceBus/StaticSubscriptionStorage.cs
@@ -8,6 +8,7 @@ namespace Machine.Mta
 {
   public class StaticSubscriptionStorage : ISubscriptionStorage
   {
+    readonly static log4net.ILog _log = log4net.LogManager.GetLogger(typeof(StaticSubscriptionStorage));
     readonly IMessageRouting _routing;
 
     public StaticSubscriptionStorage(IMessageRouting routing)
@@ -17,7 +18,22 @@ namespace Machine.Mta
 
     public void HandleSubscriptionMessage(TransportMessage msg)
     {
-      throw new NotSupportedException();
+      NServiceBus.IMessage[] messages = msg.Body;
+      if ((messages != null) && (messages.Length == 1))
+      {
+        SubscriptionMessage subMessage = messages[0] as SubscriptionMessage;
+        if (subMessage != null)
+        {
+          if (subMessage.SubscriptionType == SubscriptionType.Add)
+          {
+            _log.Info("Add: " + subMessage.TypeName + " " + msg.ReturnAddress);
+          }
+          if (subMessage.SubscriptionType == SubscriptionType.Remove)
+          {
+            _log.Info("Remove: " + subMessage.TypeName + " " + msg.ReturnAddress);
+          }
+        }
+      }
     }
 
     public IList<string> GetSubscribersForMessage(Type messageType)
