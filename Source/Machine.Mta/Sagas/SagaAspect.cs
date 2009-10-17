0a370bf86494bb31c3791936c0d25c9f2e31e3d6
Jacob Lewallen <jlewallen@gmail.com>
2009-10-16 20:36:28 -0700

    Easier to read String.Join.

--- Source/Machine.Mta/Sagas/SagaAspect.cs
+++ Source/Machine.Mta/Sagas/SagaAspect.cs
@@ -165,16 +165,7 @@ namespace Machine.Mta.Sagas
 
     public static string Join(this IEnumerable<string> values, string separator)
     {
-      StringBuilder sb = new StringBuilder();
-      foreach (string value in values)
-      {
-        if (sb.Length != 0)
-        {
-          sb.Append(separator);
-        }
-        sb.Append(value);
-      }
-      return sb.ToString();
+      return String.Join(separator, values.ToArray());
     }
   }
 }
