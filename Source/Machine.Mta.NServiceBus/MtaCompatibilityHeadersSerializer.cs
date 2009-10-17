30ab69c56c2e82e5fe9ef2f6898f4ccff506fea2
Jacob Lewallen <jlewallen@gmail.com>
2009-10-17 00:05:53 -0700

    MSB header serializer that transfers our SagaIds in an MTA friendly way.

--- Source/Machine.Mta.NServiceBus/MtaCompatibilityHeadersSerializer.cs
+++ Source/Machine.Mta.NServiceBus/MtaCompatibilityHeadersSerializer.cs
@@ -7,54 +7,37 @@ namespace Machine.Mta
 {
   public class MtaCompatibilityHeadersSerializer : IHeadersSerializer
   {
+    readonly static log4net.ILog _log = log4net.LogManager.GetLogger(typeof(MtaCompatibilityHeadersSerializer));
     readonly XmlHeadersSerializer _serializer = new XmlHeadersSerializer();
 
-    public byte[] Serialize(IEnumerable<HeaderInfo> headers)
+    public byte[] Serialize(IList<HeaderInfo> headers)
     {
-      var cmc = CurrentMessageContext.Current;
-      if (cmc != null)
+      if (CurrentSagaIds.OutgoingIds != null && CurrentSagaIds.OutgoingIds.Length > 0)
       {
-        var value = String.Join(",", cmc.SagaIds.Select(x => x.ToString()).ToArray());
-        var header = new HeaderInfo() {
+        var value = String.Join(",", CurrentSagaIds.OutgoingIds.Select(x => x.ToString()).ToArray());
+        var header = new HeaderInfo()
+        {
           Key = "machine.sagas",
           Value = value
         };
-        return _serializer.Serialize(headers.Union(new[] { header }));
+        _log.Debug("Serializing: " + value);
+        return _serializer.Serialize(headers.Union(new[] { header }).ToList());
       }
       return _serializer.Serialize(headers);
     }
 
     public HeaderInfo[] Deserialize(byte[] buffer)
     {
-      CurrentSagaIds.Use(new Guid[0]);
+      CurrentSagaIds.UseIncoming(new Guid[0]);
       var headers = _serializer.Deserialize(buffer);
       var sagaHeader = headers.SingleOrDefault(h => h.Key == "machine.sagas");
       if (sagaHeader != null)
       {
+        _log.Debug("Deserializing: " + sagaHeader.Value);
         var guids = sagaHeader.Value.Split(',').Where(x => !String.IsNullOrEmpty(x)).Select(x => new Guid(x)).ToArray();
-        CurrentSagaIds.Use(guids);
+        CurrentSagaIds.UseIncoming(guids);
       }
       return headers;
-    }
-  }
-
-  public static class CurrentSagaIds
-  {
-    [ThreadStatic]
-    public static Guid[] _sagaIds;
-
-    public static Guid[] Ids
-    {
-      get { return _sagaIds; }
-    }
-
-    public static void Use(Guid[] sagaIds)
-    {
-      _sagaIds = sagaIds;
-    }
-
-    public static void CopyToOutgoing()
-    {
     }
   }
 }
